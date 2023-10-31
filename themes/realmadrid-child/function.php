c69742f5d090d162e4eeef1a318c2f682df3428d
Muayad <moayad_nashwan@yahoo.com>
2023-10-31 00:13:34 -0400

    Adding theme child with its settings

---
+++ themes/realmadrid-child/function.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+add_action( 'wp_enqueue_scripts', 'realmadridchild_wp_enqueue_scripts' );
+function realmadridchild_wp_enqueue_scripts() {
+	wp_enqueue_style(
+        'realmadridchild-style',
+		get_stylesheet_uri(),
+		array( 'twenty-twenty-one-style' ),
+		wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
+	);
+}
