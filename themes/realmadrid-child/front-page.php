bc23cad29d24970a3c7a2269e1a8e62f9c93c877
Muayad <moayad_nashwan@yahoo.com>
2023-12-02 21:18:06 -0500

    adding custom WP_Query to the front page

--- themes/realmadrid-child/front-page.php
+++ themes/realmadrid-child/front-page.php
@@ -3,17 +3,45 @@
  * Template Name: front-page.php
  */
 
-
-
-
- get_header();
-
+get_header();
 
 /* Start the Loop */
 
+if ( is_front_page() ) {
+    $args = array(
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'posts_per_page' => 3,
+        'orderby' => 'date',
+        'order' => 'DESC',
+    );
 
+    $the_query = new WP_Query($args);
+
+    if ($the_query->have_posts()) {
+        while ($the_query->have_posts()) {
+            $the_query->the_post();
+            if (has_post_thumbnail()) {
+                the_post_thumbnail();
+            }
+            the_title();
+            ?>
+            <br>
+            <?php
+            the_excerpt();
+            ?>
+            <br>
+            <?php
+            echo '<a href="' . get_permalink() . '">Read More</a>';?>
+            <br>
+            <?php
+        }
+        wp_reset_postdata();
+    } else {
+        echo 'No posts found';
+    }
+}
 
 get_footer();
-//helloworldss
-//adding another comment
+
 ?>
