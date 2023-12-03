92cce3a0b8ba78ca12a627340cf2fc388b64eb01
Muayad <moayad_nashwan@yahoo.com>
2023-12-02 21:29:11 -0500

    adding the custom query with the tag statements to the file

--- themes/realmadrid-child/templates/custom-template.php
+++ themes/realmadrid-child/templates/custom-template.php
@@ -8,7 +8,44 @@
 
 
  get_header();
-
+ 
+ /* Start the Loop */
+    if (is_front_page() || is_page('about')) {
+        $args = array(
+            'post_type' => 'post',
+            'post_status' => 'publish',
+            'posts_per_page' => 3,
+            'orderby' => 'date',
+            'order' => 'DESC',
+        );
+    
+        $the_query = new WP_Query($args);
+    
+        if ($the_query->have_posts()) {
+            
+            while ($the_query->have_posts()) {
+                $the_query->the_post();
+                if (has_post_thumbnail()) {
+                    the_post_thumbnail();
+                }
+                the_title();
+                ?>
+                <br>
+                <?php
+                the_excerpt();
+                ?>
+                <br>
+                <?php
+                echo '<a href="' . get_permalink() . '">Read More</a>';?>
+                <br>
+                <?php
+            }
+            wp_reset_postdata();
+        } else {
+            echo 'No posts found';
+        }
+    }
+ 
 
 
 
