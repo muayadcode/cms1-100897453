7daafa90c8bd949d770ff8615fa3f8f584ebc608
Muayad <moayad_nashwan@yahoo.com>
2023-12-04 01:35:22 -0500

    edit front page template

--- themes/realmadrid-child/front-page.php
+++ themes/realmadrid-child/front-page.php
@@ -22,18 +22,19 @@ if ( is_front_page() ) {
         while ($the_query->have_posts()) {
             $the_query->the_post();
             if (has_post_thumbnail()) {
-                the_post_thumbnail();
+                echo '<img class="custom-class" src="' . esc_url(get_the_post_thumbnail_url()) . '" alt="' . esc_attr(get_the_title()) . '">';
             }
-            the_title();
+            
             ?>
-            <br>
+            <h2 class ="customH"> <?php the_title()?> </h2>
+           
             <?php
-            the_excerpt();
+            echo '<p class = myP>', get_the_excerpt(), '</p>';
             ?>
-            <br>
+            
             <?php
-            echo '<a href="' . get_permalink() . '">Read More</a>';?>
-            <br>
+            echo '<a class= "cA" href="' . get_permalink() . '">Read More</a>';?>
+            
             <?php
         }
         wp_reset_postdata();
@@ -41,6 +42,9 @@ if ( is_front_page() ) {
         echo 'No posts found';
     }
 }
+
+
+
 
 get_footer();
 
