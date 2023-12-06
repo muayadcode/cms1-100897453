c3bbc546e19d41b2fb414ed6dbee7653bcf6d40c
Muayad <moayad_nashwan@yahoo.com>
2023-12-05 20:26:44 -0500

    add changes to the files

--- themes/realmadrid-child/footer.php
+++ themes/realmadrid-child/footer.php
@@ -12,10 +12,48 @@
  */
 
 ?>
+
 			</main><!-- #main -->
 		</div><!-- #primary -->
 	</div><!-- #content -->
-
+    <?php
+    if (is_front_page() || is_page('about-us')) {
+        $args = array(
+            'post_type' => 'post',
+            'post_status' => 'publish',
+            'posts_per_page' => 3,
+            'orderby' => 'date',
+            'order' => 'DESC',
+        );
+    
+        $the_query = new WP_Query($args);
+    
+        if ($the_query->have_posts()) {
+            
+            while ($the_query->have_posts()) {
+                $the_query->the_post();
+                if (has_post_thumbnail()) {
+                    echo '<img class="custom-class" src="' . esc_url(get_the_post_thumbnail_url()) . '" alt="' . esc_attr(get_the_title()) . '">';
+                }
+                
+                ?>
+                <h2 class ="customH"> <?php the_title()?> </h2>
+               
+                <?php
+                echo '<p class = myP>', get_the_excerpt(), '</p>';
+                ?>
+                
+                <?php
+                echo '<a class= "cA" href="' . get_permalink() . '">Read More</a>';?>
+                
+                <?php
+            }
+            wp_reset_postdata();
+        } else {
+            echo 'No posts found';
+        }
+    }
+    ?>
 	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
 
 	<footer id="colophon" class="site-footer">
--- themes/realmadrid-child/front-page.php
+++ themes/realmadrid-child/front-page.php
@@ -9,41 +9,6 @@ get_header();
 <?php
 /* Start the Loop */
 
-if ( is_front_page() ) {
-    $args = array(
-        'post_type' => 'post',
-        'post_status' => 'publish',
-        'posts_per_page' => 3,
-        'orderby' => 'date',
-        'order' => 'DESC',
-    );
-
-    $the_query = new WP_Query($args);
-
-    if ($the_query->have_posts()) {
-        while ($the_query->have_posts()) {
-            $the_query->the_post();
-            if (has_post_thumbnail()) {
-                echo '<img class="custom-class" src="' . esc_url(get_the_post_thumbnail_url()) . '" alt="' . esc_attr(get_the_title()) . '">';
-            }
-            
-            ?>
-            <h2 class ="customH"> <?php the_title()?> </h2>
-           
-            <?php
-            echo '<p class = myP>', get_the_excerpt(), '</p>';
-            ?>
-            
-            <?php
-            echo '<a class= "cA" href="' . get_permalink() . '">Read More</a>';?>
-            
-            <?php
-        }
-        wp_reset_postdata();
-    } else {
-        echo 'No posts found';
-    }
-}
 
 
 
